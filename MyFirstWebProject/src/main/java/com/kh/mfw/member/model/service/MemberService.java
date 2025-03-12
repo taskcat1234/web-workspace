a9d49d2552322611012bd06906646afbdc1cf5ac
taskcat1234 <1232168@naver.com>
2025-03-12 17:35:52 +0900

    2025-03-12 (최종) 홈페이지 로그인 로그아웃 만들기

---
+++ MyFirstWebProject/src/main/java/com/kh/mfw/member/model/service/MemberService.java
@@ -0,0 +1,37 @@
+package com.kh.mfw.member.model.service;
+
+import com.kh.mfw.member.model.dao.MemberDAO;
+import com.kh.mfw.member.model.dto.MemberDTO;
+
+public class MemberService {
+	
+	public MemberDTO login(MemberDTO member) {
+		
+		/*
+		  로그인을하는건데
+		  로그인이라 함은
+		  DB에가서 SELECT절에다가 사용자가 입력한 아이디값과 비밀번호값을 조건절에 사용해서 조회를 하는 절차를 의미
+		  
+		  ID가 틀리거나 PW가 틀려서 로그인에 실패할 수 있는 가능성이 있음
+		
+		
+		member.getMemberId().length() > 10
+		member.getMemberId().equals("")
+		
+		위에 조건이 틀리면 DB에 갈 필요 자체가 없다
+		서비스 단에서 유효성을 검사하기 위함 (DB에러 요소의 가능성을 차단)
+		또한 DB연결 세션 갯수에 제한이 있어 지연 시간이 늘어나는걸 방지하기 위함
+		
+		*/
+		
+		MemberDTO loginMember = new MemberDAO().login(member);
+		return loginMember;
+		
+		
+		
+		
+		
+		
+	}
+	
+}
---
+++ MyFirstWebProject/src/main/webapp/index.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>월컴 메인 화면</title>
+</head>
+<body>
+
+	<!--  
+		* WEB환경에서의 CRUD를 구현
+		
+		* 회원서비스
+		로그인(R),회원가입(C)
+	-->
+	
+	<jsp:include page="WEB-INF/views/include/header.jsp" />
+	<jsp:include page="WEB-INF/views/include/main.jsp" />
+	<jsp:include page="WEB-INF/views/include/footer.jsp" />
+
+</body>
+</html>
